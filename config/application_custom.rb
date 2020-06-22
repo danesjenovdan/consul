34d2adcf9f06ca486415bb24ac530d593f82980d
muki <filipdobranic@gmail.com>
2020-06-22 08:06:17 +0200

    remove en from available locales

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -3,7 +3,7 @@ module Consul
     config.i18n.default_locale = 'sl'
     # available_locales = %w(ar de en es fa fr gl he it nl pl pt-BR sq sv val zh-CN zh-TW sl sl-SI)
     # TODO nekega dne je treba pogruntati zakaj rabiš angleški locale, da postaviš sceno
-    available_locales = %w(sl it en)
+    available_locales = %w(sl it)
     config.i18n.available_locales = available_locales
     config.i18n.fallbacks = {
       # 'fr' => 'es',
