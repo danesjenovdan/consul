26af6b3ed32074e12ff658a33b60ef29db8b51ce
muki <filipdobranic@gmail.com>
2020-08-18 21:28:50 +0200

    fix documentable bug

--- app/models/custom/concerns/documentable.rb
+++ app/models/custom/concerns/documentable.rb
@@ -3,11 +3,6 @@ require_dependency Rails.root.join('app', 'models', 'concerns', 'documentable').
 module Documentable
     extend ActiveSupport::Concern
   
-    included do
-      has_many :documents, as: :documentable, inverse_of: :documentable, dependent: :destroy
-      accepts_nested_attributes_for :documents, allow_destroy: true
-    end
-  
     module ClassMethods
       def max_documents_allowed
         10
