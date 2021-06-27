818c3bcaa1f8f0e5f8aaafc9a82655eac40f2812
kevin <kevin.cvetezar@gmail.com>
2021-06-27 18:03:58 +0200

    Refactor custom_phase_helper to budget method.

--- app/controllers/custom/welcome_controller.rb
+++ app/controllers/custom/welcome_controller.rb
@@ -1,15 +1,10 @@
 require_dependency Rails.root.join('app', 'controllers', 'welcome_controller').to_s
 
 class WelcomeController < ApplicationController
-  include CustomPhasesHelper
   skip_authorization_check
 
-  before_action :load_custom_phases, only: [:index]
+  before_action only: [:index]
 
   layout "devise", only: [:verification]
-
-  def index
-    @custom_phases = custom_phases
-  end
 
 end
--- app/models/custom/budget.rb
+++ app/models/custom/budget.rb
@@ -1,233 +1,42 @@
+require_dependency Rails.root.join('app', 'models', 'budget').to_s
+
 class Budget < ApplicationRecord
-    include Measurable
-    include Sluggable
-    include StatsVersionable
-    include Reportable
-  
-    translates :name, touch: true
-    include Globalizable
-  
-    class Translation
-      validate :name_uniqueness_by_budget
-  
-      def name_uniqueness_by_budget
-        if Budget.joins(:translations)
-                 .where(name: name)
-                 .where.not("budget_translations.budget_id": budget_id).any?
-          errors.add(:name, I18n.t("errors.messages.taken"))
-        end
-      end
-    end
-  
-    CURRENCY_SYMBOLS = %w[€ $ £ ¥].freeze
-    VOTING_STYLES = %w[knapsack approval].freeze
-  
-    validates_translation :name, presence: true
-    validates :phase, inclusion: { in: Budget::Phase::PHASE_KINDS }
-    validates :currency_symbol, presence: true
-    validates :slug, presence: true, format: /\A[a-z0-9\-_]+\z/
-    validates :voting_style, inclusion: { in: VOTING_STYLES }
-  
-    has_many :investments, dependent: :destroy
-    has_many :ballots, dependent: :destroy
-    has_many :groups, dependent: :destroy
-    has_many :headings, through: :groups
-    has_many :lines, through: :ballots, class_name: "Budget::Ballot::Line"
-    has_many :phases, class_name: "Budget::Phase"
-    has_many :budget_administrators, dependent: :destroy
-    has_many :administrators, through: :budget_administrators
-    has_many :budget_valuators, dependent: :destroy
-    has_many :valuators, through: :budget_valuators
-  
-    has_one :poll
-  
-    after_create :generate_phases
-  
-    scope :published, -> { where(published: true) }
-    scope :drafting,  -> { where.not(id: published) }
-    scope :informing, -> { where(phase: "informing") }
-    scope :accepting, -> { where(phase: "accepting") }
-    scope :reviewing, -> { where(phase: "reviewing") }
-    scope :selecting, -> { where(phase: "selecting") }
-    scope :valuating, -> { where(phase: "valuating") }
-    scope :valuating_or_later, -> { where(phase: Budget::Phase.kind_or_later("valuating")) }
-    scope :publishing_prices, -> { where(phase: "publishing_prices") }
-    scope :balloting, -> { where(phase: "balloting") }
-    scope :reviewing_ballots, -> { where(phase: "reviewing_ballots") }
-    scope :finished, -> { where(phase: "finished") }
-  
-    class << self; undef :open; end
-    scope :open, -> { where.not(phase: "finished") }
-  
-    def self.current
-        where.not(phase: "drafting").order(:created_at).last
-    end
-  
-    def current_phase
-      phases.send(phase)
-    end
-  
-    def published_phases
-      phases.published.order(:id)
-    end
-  
-    def starts_at
-      phases.published.first&.starts_at
-    end
-  
-    def ends_at
-      phases.published.last&.ends_at
-    end
-  
-    def description
-      description_for_phase(phase)
-    end
-  
-    def description_for_phase(phase)
-      if phases.exists? && phases.send(phase).description.present?
-        phases.send(phase).description
-      else
-        send("description_#{phase}")
-      end
-    end
-  
-    def self.title_max_length
-      80
-    end
-  
-    def publish!
-      update!(published: true)
-    end
-  
-    def drafting?
-      !published?
-    end
-  
-    def informing?
-      phase == "informing"
-    end
-  
-    def accepting?
-      phase == "accepting"
-    end
-  
-    def reviewing?
-      phase == "reviewing"
-    end
-  
-    def selecting?
-      phase == "selecting"
-    end
-  
-    def valuating?
-      phase == "valuating"
-    end
-  
-    def publishing_prices?
-      phase == "publishing_prices"
-    end
-  
-    def balloting?
-      phase == "balloting"
-    end
-  
-    def reviewing_ballots?
-      phase == "reviewing_ballots"
-    end
-  
-    def finished?
-      phase == "finished"
-    end
-  
-    def published_prices?
-      Budget::Phase::PUBLISHED_PRICES_PHASES.include?(phase)
-    end
-  
-    def valuating_or_later?
-      current_phase&.valuating_or_later?
-    end
-  
-    def publishing_prices_or_later?
-      current_phase&.publishing_prices_or_later?
-    end
-  
-    def balloting_process?
-      balloting? || reviewing_ballots?
-    end
-  
-    def balloting_or_later?
-      current_phase&.balloting_or_later?
-    end
-  
-    def heading_price(heading)
-      heading_ids.include?(heading.id) ? heading.price : -1
-    end
-  
-    def formatted_amount(amount)
-      ActionController::Base.helpers.number_to_currency(amount,
-                                                        precision: 0,
-                                                        locale: I18n.locale,
-                                                        unit: currency_symbol)
-    end
-  
-    def formatted_heading_price(heading)
-      formatted_amount(heading_price(heading))
-    end
-  
-    def investments_orders
-      case phase
-      when "accepting", "reviewing"
-        %w[random]
-      when "publishing_prices", "balloting", "reviewing_ballots"
-        %w[random price]
-      when "finished"
-        %w[random]
-      else
-        %w[random confidence_score]
-      end
-    end
-  
-    def email_selected
-      investments.selected.order(:id).each do |investment|
-        Mailer.budget_investment_selected(investment).deliver_later
-      end
-    end
-  
-    def email_unselected
-      investments.unselected.order(:id).each do |investment|
-        Mailer.budget_investment_unselected(investment).deliver_later
-      end
-    end
-  
-    def has_winning_investments?
-      investments.winners.any?
-    end
-  
-    def investments_milestone_tags
-      investments.winners.map(&:milestone_tag_list).flatten.uniq.sort
-    end
-  
-    def approval_voting?
-      voting_style == "approval"
-    end
-  
-    private
-  
-      def generate_phases
-        Budget::Phase::PHASE_KINDS.each do |phase|
-          Budget::Phase.create(
-            budget: self,
-            kind: phase,
-            name: I18n.t("budgets.phase.#{phase}"),
-            prev_phase: phases&.last,
-            starts_at: phases&.last&.ends_at || Date.current,
-            ends_at: (phases&.last&.ends_at || Date.current) + 1.month
-          )
-        end
-      end
-  
-      def generate_slug?
-        slug.nil? || drafting?
-      end
+
+  CUSTOM_PHASE_ACCEPTING = :accepting
+  CUSTOM_PHASE_SELECTING = :selecting
+  CUSTOM_PHASE_BALLOTING = :balloting
+  CUSTOM_PHASE_FINISHED = :finished
+
+  CustomPhase = Struct.new(:kind, :summary, :presentation_summary_1, :presentation_summary_2, :presentation_summary_3, :description, :starts_at, :ends_at, :url, :enabled) do
+
+  end
+
+  def self.current
+    where.not(phase: "drafting").order(:created_at).last
   end
   
+  def custom_phases(current_user, budget_investments_url)
+    custom_phases = {}
+    {
+      CUSTOM_PHASE_ACCEPTING => true,
+      CUSTOM_PHASE_SELECTING => self.phases.selecting.enabled?,
+      CUSTOM_PHASE_BALLOTING => true,
+      CUSTOM_PHASE_FINISHED => true,
+    }.each do |phase, enabled|      
+      current_phase = self.phases.public_send(phase)
+      url = nil
+      if self.phase === phase.to_s
+        if self.phase === 'balloting'
+         url = budget_investments_url.call(self, heading_id: current_user  && current_user.balloted_heading_id ? current_user.balloted_heading_id : self.headings.first.id)
+        else
+         url = budget_investments_url.call(self)
+        end
+      end
+      print url
+      custom_phases[phase] = CustomPhase.new(phase, current_phase.summary, current_phase.presentation_summary_1,
+      current_phase.presentation_summary_2, current_phase.presentation_summary_2,
+      current_phase.description, current_phase.starts_at, current_phase.ends_at, url, enabled)
+    end
+    custom_phases
+  end
+end
