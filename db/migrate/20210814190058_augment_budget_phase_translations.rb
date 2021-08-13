65b480562be3186d95b214b507bb14f3dcba0152
kevin <kevin.cvetezar@gmail.com>
2021-08-13 13:19:41 +0200

    Fix linter issues

--- db/migrate/20210814190058_augment_budget_phase_translations.rb
+++ db/migrate/20210814190058_augment_budget_phase_translations.rb
@@ -1,8 +1,7 @@
 class AugmentBudgetPhaseTranslations < ActiveRecord::Migration[4.2]
-    def change
-      add_column :budget_phase_translations, :presentation_summary_1, :string
-      add_column :budget_phase_translations, :presentation_summary_2, :string
-      add_column :budget_phase_translations, :presentation_summary_3, :string
-    end
+  def change
+    add_column :budget_phase_translations, :presentation_summary_1, :string
+    add_column :budget_phase_translations, :presentation_summary_2, :string
+    add_column :budget_phase_translations, :presentation_summary_3, :string
   end
-  
+end
