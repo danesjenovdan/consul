1ced46a14183ec0ba85f20a34ca7437b192c3c61
muki <filipdobranic@gmail.com>
2021-08-19 19:27:44 +0200

    set locales (default sl, also available it, hu-HU)

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,11 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :sl
+    available_locales = [
+      "sl",
+      "it",
+      "hu-HU"
+    ]
+    config.i18n.available_locales = available_locales
   end
 end
