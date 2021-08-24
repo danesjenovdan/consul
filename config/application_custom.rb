47617c8b263b34e2d17806406d5c1fcffe84ca5e
muki <filipdobranic@gmail.com>
2021-08-24 12:29:29 +0200

    another hopeful attempt

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -7,5 +7,10 @@ module Consul
       "hu-HU"
     ]
     config.i18n.available_locales = available_locales
+
+    # Set different path for compiled assets in production.
+    # This is required so we can properly mount the volume
+    # in several different containers.
+    config.assets.prefix = "../../../../pvc"
   end
 end
