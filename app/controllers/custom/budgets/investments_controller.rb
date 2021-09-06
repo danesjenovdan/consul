5ea3970560739ab617c5b959e1a8da927a70b8ff
kevin <kevin.cvetezar@gmail.com>
2021-09-06 09:31:30 +0200

    Add switcher for consul versions

--- app/controllers/custom/budgets/investments_controller.rb
+++ app/controllers/custom/budgets/investments_controller.rb
@@ -3,9 +3,14 @@ require_dependency Rails.root.join("app", "controllers", "budgets", "investments
 module Budgets
   class InvestmentsController < ApplicationController
     before_action :load_categories, only: [:index, :new, :create, :edit, :update]
+    before_action :load_budgets,  only: [:index, :new, :create, :edit, :update]
 
     has_orders %w[most_voted newest], only: :show
     has_orders ->(c) { c.investments_orders }, only: :index
+
+    def load_budgets
+      @budgets = Budget.where("id > -1");
+    end
 
     def index
       @investments_count = investments.count
