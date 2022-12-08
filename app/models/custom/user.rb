c6595b309f1f3e6ed6b2530e7ab659dcbc2f958c
muki <filipdobranic@gmail.com>
2022-12-08 14:48:55 +0100

    update medvode age limit

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -222,7 +222,7 @@ class User < ApplicationRecord
   end
 
   def is_old_enough(birthday)
-    date_limit = Date.parse('2005-12-13')
+    date_limit = Date.parse('2007-12-10')
     if date_limit < birthday
       return false
     end
