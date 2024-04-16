13ab0a3bc8bec0de3da274215b91d2718bd7cb14
Patricija Brečko <patricija@danesjenovdan.si>
2024-04-16 12:02:18 +0200

    update age limit

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   end
 
   def is_old_enough(birthday)
-    date_limit = Date.parse('2008-09-20')
+    date_limit = Date.parse('2009-05-22')
     if date_limit < birthday
       return false
     end
